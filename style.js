699ab35d6f5e8be9026ace1410485db5ee00da92
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2022-02-06 15:26:26 +0100

    15th commit

--- style.js
+++ style.js
@@ -3,7 +3,6 @@ function set_Style(){
     height = window.innerHeight;
     console.log(width);
     if (width < 900){
-        document.getElementsByClassName("grid_Div").style.width = `${width/2}px`;
         document.getElementsByClassName("title").style = "font-size: 20px";
     }
 }
