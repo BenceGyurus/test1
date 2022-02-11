a72d03a9604ec690b04162f4bf7db6fbdc9ddf10
BenceGyurus <81524938+BenceGyurus@users.noreply.github.com>
2022-02-11 22:25:55 +0100

    new 1.0

--- main.js
+++ main.js
@@ -1,26 +1,5 @@
 var answers = [];
 
-class Html_Items{
-    static div(id, datas, class_Name){
-        return `<div id = "${id}" class = "${class_Name}">${datas}</div>`;
-    }
-    static h_Tag(id, data, class_Name,number_Of_H){
-        return `<h${number_Of_H} id = "${id}" class = "${class_Name}">${data}</h${number_Of_H}>`;
-    }
-    static inputs(id, value, class_Name,name,type_Of_Input, placeholder){
-        return `<input type = "${type_Of_Input}" id = "${id}" class = "${class_Name}" value = "${value}" name = "${name}" placeholder = "${placeholder}"/>`;
-    }
-    static label(id, value, class_Name, for_This){
-        return `<label id = "${id}" class = "${class_Name}" for = "${for_This}">${value}</label>`;
-    }
-    static radio_Button(id, data, class_Name, name, value){
-        return this.div("", this.label("", data, "radiobutton_Question")+this.inputs(id, value, "radiobutton", name, "radio"), class_Name);
-    }
-    static p_Tag(id, data, class_Name){
-        return `<p id = "${id}" class = "${class_Name}">${data}</p>`;
-    }
-}
-
 function random_Id(length){
     x = "";
     for (let i = 0; i<length; i++){
